aba9167dd040e1fedc010323f38821a5abddc4b5
bojnansky <bojnansky10@gmail.com>
2024-04-17 19:50:07 +0200

    Úprava profilingu

--- src/profiling.py
+++ src/profiling.py
@@ -1,7 +1,7 @@
 ## 
 # @file profiling.py
 # @author Adam Bojnanský
-# @brief Profiling pre kalkulačku
+# @brief Program na výpočet výberovej smerodatnej odchýlky pre kalkulačku
 import sys
 from calc import *
 
@@ -13,33 +13,15 @@ bracket = 0
 fraction = 0
 s = 0
 
-if len(sys.argv) > 1:
-    # Ak bol zadaný súbor ako argument príkazového riadku
-    subor = sys.argv[1]
-    with open(subor, 'r') as file:
-        for riadok in file:
-            cisla = riadok.split()
-            # Vypíšeme každé číslo zo získaného zoznamu
-            for cislo in cisla:
-                sum = add(sum, float(cislo))
-                sum_2 = add(sum_2, pow(float(cislo), 2))
-                N += 1
+for line in sys.stdin:
+    numbers = line.split()
+    for number in numbers:
+        sum = add(sum, float(number))
+        sum_2 = add(sum_2, pow(float(number), 2))
+        N = add(N, 1)
 
-    x = div(sum, N)
-    bracket = sub(sum_2, mult(N, pow(x, 2)))
-    fraction = div(1, sub(N, 1))
-    s = root(mult(fraction, bracket), 2)
-    print("sum: %d" % sum)
-    print("sum_2: %d" % sum_2)
-    print("N: %d" % N)
-    print("x: %f" % x)
-    print("bracket: %f" % bracket)
-    print("fraction: %f" % fraction)
-    print(s)
-
-
-else:
-    # Inak čítame zo štandardného vstupu
-    for cislo in sys.stdin:
-        # Spracovanie riadku zo štandardného vstupu
-        print(cislo)
+x = div(sum, N)
+bracket = sub(sum_2, mult(N, pow(x, 2)))
+fraction = div(1, sub(N, 1))
+s = root(mult(fraction, bracket), 2)
+print(s)
