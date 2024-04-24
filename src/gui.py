cc63625973a8d5c8f8c4dd513c521954991f9bd6
xmihalm00 <xmihalm00@stud.fit.vutbr.cz>
2024-04-24 16:57:25 +0200

    Úprava komentárov GUI

--- src/gui.py
+++ src/gui.py
@@ -23,7 +23,7 @@ window = Tk()
 
 window.geometry("500x700")
 window.configure(bg = "#808080")
-window.title("Kalcoolačka")
+window.title("LPH Kalkulačka")
 
 
 canvas = Canvas(
@@ -68,12 +68,13 @@ cond = 0
 cond2 = 0
 comma = ""
 
+##
 # @brief Stlačenie tlačidla 
 #
 # Pri každom stlačení niektorého z tlačidiel sa pomocu funkcie button_click pošle danný string a ten sa následne vytlačí 
 # po každom pridaní daľšieho znaku
 #
-# @param funkcia dostáva string 
+# @param number číslo 
 
 def button_click(number):
     global cond2
@@ -89,12 +90,13 @@ def button_click(number):
         e.insert(0,current + number + comma)
     cond2=0
 
+##
 # @brief Stlačenie tlačidla logaritmus 
 #
 # Po stlační logaritmu sa vytlačí log( a nastavý sa podmienka vypísania čiarky "cond2" je to kvoli parseru ten berie logaritmus 
 # ako log(a,b) kde a je zaklad a b je argument
 #
-# @param funkcia dostáva string 
+# @param number základ logaritmu 
 
 def button_logaritm(number):
     global cond2
@@ -102,13 +104,14 @@ def button_logaritm(number):
     current = e.get()
     e.delete(0, END)
     e.insert(0,current + number )
-    
+
+##    
 # @brief Stlačenie tlačidla answer
 #
 # Po stlační tlačidla answer sa vytlačí odpoveď s predošlého výpočtu. Riadiaca podmienka sa vyhodnocuje v funkcii button_equal 
 # pretože na žiatku je výsledok prázdny string
 #
-# @param funkcia dostáva string 
+# @param ans funkcia dostáva string 
 
 def button_answer(ans):
     new_ans=""
@@ -124,26 +127,26 @@ def button_answer(ans):
         new_ans=ans
     else:
         e.insert(0,new_ans)
-        
+
+
+##        
 # @brief Stlačenie tlačidla delete
 #
 # Po stlační tlačidla delete odstráni posledný znak z pola a vymaže ho z okna. 
-#
-# @param funkcia dostáva string 
-
+# 
 def button_delete():
     global ans
     ans = ans[:-1]
     e.delete(0, END)
     e.insert(0,ans)
 
+##
 # @brief Stlačenie tlačidla rovná sa
 #
 # Po stlační tlačidla rovná sa , to posiela string parseru ktorý ho vyhodnotí a pošle string späť.
 #
-# @param funkcia posiela string
+# @param ans predošlý výraz
 # @return funkcia evaluate vracia string
-
 def button_equal(ans):
     try:
         result = parse.evaluate(ans)
@@ -154,12 +157,11 @@ def button_equal(ans):
     e.delete(0, END)
     e.insert(0,str(result))
 
+##
 # @brief Stlačenie tlačidla clear all
 #
-# Po stlační tlačidla ac sa vymaže obsah celého okna
+# Po stlačení tlačidla ac sa vymaže obsah celého okna
 #
-
-
 def button_clear():
     e.delete(0, END)
 
